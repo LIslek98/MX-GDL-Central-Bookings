35981da5ede586195f1cbb1c5b39624afb69b4c9
DavidTMN <davidtormon@gmail.com>
2025-11-20 14:16:59 +0800

    added max_length to participant_type field in Participant

--- centralbookings/Profile/migrations/0001_initial.py
+++ centralbookings/Profile/migrations/0001_initial.py
@@ -82,7 +82,7 @@ class Migration(migrations.Migration):
                 ('participant_ID', models.AutoField(primary_key=True, serialize=False)),
                 ('name', models.CharField(max_length=255)),
                 ('birth_date', models.DateField()),
-                ('participant_type', models.CharField()),
+                ('participant_type', models.CharField(max_length=255)),
                 ('department', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to='Profile.department')),
             ],
         ),
