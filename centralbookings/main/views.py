b6681e16b8dba22a76277a219167173005b9d418
DavidTMN <davidtormon@gmail.com>
2025-11-20 14:35:17 +0800

    Added placeholder code for activity_create

--- centralbookings/main/views.py
+++ centralbookings/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.contrib.auth.decorators import login_required
 from Profile.models import Activity_Schedule
 
 def activity_list(request):
@@ -10,9 +11,29 @@ def activity_list(request):
     }
     return render(request, 'activitylist.html', context)
 
+@login_required
 def activity_create(request):
-    scheds = Activity_Schedule.objects.all()
-    context = {
-        'activities': scheds
-    }
-    return render(request, 'activitylist.html', context)
+    # form = CreateForm()
+    
+    # if request.method == 'POST':
+    #     form = CreateForm(request.POST)
+    #     formset = CreateFormSet(request.POST, queryset=Job.objects.none())
+
+    #     if form.is_valid():
+    #         commission = form.save(commit=False)
+    #         commission.author = request.user.profile
+    #         commission.save()
+
+    #         if formset.is_valid():
+    #             for job_form in formset:
+    #                 if job_form.cleaned_data:
+    #                     job = job_form.save(commit=False)
+    #                     job.commission = commission
+    #                     job.save()
+
+    #         return redirect('commissions:commission_detail', pk=commission.pk)
+    # else:
+    #     form = CommissionForm()
+    #     formset = JobFormSet(queryset=Job.objects.none())
+
+    # return render(request, 'commission_form.html', {'commission_form': form,'job_formset': formset})
