b57acb1e2c524ee8d07f4ed20b8f6b210696b5de
DavidTMN <davidtormon@gmail.com>
2025-11-28 23:01:24 +0800

    modified forms to reflect changes in models

--- centralbookings/UserManagement/forms.py
+++ centralbookings/UserManagement/forms.py
@@ -3,6 +3,11 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Organizer, Participant, Contact_Person
 
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import Organizer, Participant, Contact_Person
+
 class UserForm(UserCreationForm):
     class Meta:
         model = User
@@ -12,16 +17,23 @@ class UserForm(UserCreationForm):
 class ParticipantForm(forms.ModelForm):
     class Meta:
         model = Participant
-        fields = ['name', 'birth_date', 'participant_type', 'department']
-
+        fields = [
+            'first_name', 'middle_name', 'last_name', 
+            'birth_date', 'participant_type', 'department'
+            ]
 
 class OrganizerForm(forms.ModelForm):
     class Meta:
         model = Organizer
-        fields = ['name', 'organizer_type', 'address']
-
+        fields = [
+            'name', 'organizer_type', 'street',
+            'barangay', 'city', 'region'
+            ]
 
 class ContactPersonForm(forms.ModelForm):
     class Meta:
         model = Contact_Person
-        fields = ["contact_name", "contact_email", "contact_number"]
+        fields = [
+            'first_name', 'middle_name', 'last_name', 
+            'contact_email', 'contact_number'
+            ]
