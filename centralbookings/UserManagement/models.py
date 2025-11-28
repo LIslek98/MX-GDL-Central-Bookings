692c2c3b42cead86546d3ca8675fd9d053087f79
Mady <youngmadeleineh@gmail.com>
2025-11-28 19:07:17 +0800

    Add all local files

---
+++ centralbookings/UserManagement/models.py
@@ -0,0 +1,87 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Contact_Person(models.Model):
+    contact_person_id = models.AutoField(primary_key=True)
+    contact_name = models.CharField(max_length=255)
+    contact_email = models.EmailField(max_length=255)
+    contact_number = models.IntegerField()          #this is so ugly ngl
+    def __str__(self):
+        """Return the name of the Contact Person."""
+        return self.contact_name
+
+
+class Organizer(models.Model):
+    """Create ArticleCategory with appropriate field, sorted alphabetically."""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    organizer_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    organizer_type = models.CharField(max_length=255)
+    contact_person = models.ForeignKey(Contact_Person, on_delete=models.SET_NULL, null=True)#idk how to connect foreign key to primary key, I think with django you just connect foreign key to entity? This is originally contact_person_id
+    address = models.CharField(max_length=255)
+    def __str__(self):
+        """Return the name of the Article Category."""
+        return self.name
+    class Meta:
+        """Order the article categorys alphabetically."""
+        ordering = ["name"]
+
+class Room(models.Model):
+    room_id = models.AutoField(primary_key=True)
+    room_name = models.CharField(max_length=255)
+    max_capacity = models.IntegerField()
+    def __str__(self):
+        return self.room_name
+
+
+class Location(models.Model):
+    location_id = models.AutoField(primary_key=True)
+    building = models.CharField(max_length=255) #building_name
+    room = models.ForeignKey(Room, on_delete=models.SET_NULL, null=True)
+    def __str__(self):
+        return self.building
+
+class Activity(models.Model):
+    activity_id = models.AutoField(primary_key=True)
+    activity_name = models.CharField(max_length=255)
+    def __str__(self):
+        return self.activity_name
+    
+class Activity_Schedule(models.Model):
+    schedule_ID = models.AutoField(primary_key=True)
+    date = models.DateField()       #IDK IF THIS WORKS AS A COMPOSITE
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    expected_participants = models.IntegerField(default=0)
+    organizer = models.ForeignKey(Organizer,  on_delete=models.SET_NULL, null=True)
+    location = models.ForeignKey(Location,  on_delete=models.SET_NULL, null=True)
+    activity = models.ForeignKey(Activity,  on_delete=models.SET_NULL, null=True)
+    def __str__(self):
+        return f"{self.activity} on {self.date} at {self.start_time}"
+
+class Department(models.Model):
+    department_ID = models.AutoField(primary_key=True)
+    department_name = models.CharField(max_length=255)
+    def __str__(self):
+        return self.department_name
+
+class Participant(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    participant_ID=models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    birth_date = models.DateField()
+    participant_type = models.CharField(max_length=255)                   #PLACEHOLDER: IDK HOW TO implement the subtype supertype thing
+    department = models.ForeignKey(Department, on_delete=models.SET_NULL, null=True)
+    def __str__(self):
+        return self.name
+
+class Activity_Booking(models.Model):
+    booking_ID = models.AutoField(primary_key=True)
+    has_attended = models.BooleanField()
+    booking_date = models.DateField()
+    schedule = models.ForeignKey(Activity_Schedule, on_delete=models.SET_NULL, null=True)
+    participant = models.ForeignKey(Participant, on_delete=models.SET_NULL, null=True)
+    def __str__(self):
+        return f"{self.participant} booking for {self.schedule}"
+
+
---
+++ centralbookings/UserManagement/views.py
@@ -0,0 +1,76 @@
+from django.contrib.auth.models import User
+from django.views.generic import ListView
+from django.views import View
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
+
+from .models import Organizer, Participant
+from .forms import UserForm, ParticipantForm, OrganizerForm, ContactPersonForm
+
+class UserListView(ListView):
+    model = User
+    template_name = 'register.html'
+
+class ParticipantRegisterView(View):
+    template_name = "participant_register.html"
+    success_url = reverse_lazy('UserManagement:register')
+
+    def get(self, request):
+        return render(request, self.template_name, {
+            "user_form": UserForm(),
+            "participant_form": ParticipantForm()
+        })
+
+    def post(self, request):
+        user_form = UserForm(request.POST)
+        participant_form = ParticipantForm(request.POST)
+
+        if user_form.is_valid() and participant_form.is_valid():
+            user = user_form.save()
+
+            participant = participant_form.save(commit=False)
+            participant.user = user
+            participant.save()
+
+            return redirect(self.success_url)
+
+        return render(request, self.template_name, {
+            "user_form": user_form,
+            "participant_form": participant_form
+        })
+
+class OrganizerRegisterView(View):
+    template_name = "organizer_register.html"
+    success_url = reverse_lazy('UserManagement:register')
+
+    def get(self, request):
+        return render(request, self.template_name, {
+            "user_form": UserForm(),
+            "contact_form": ContactPersonForm(),
+            "organizer_form": OrganizerForm()
+        })
+
+    def post(self, request):
+        user_form = UserForm(request.POST)
+        contact_form = ContactPersonForm(request.POST)
+        organizer_form = OrganizerForm(request.POST)
+
+        if user_form.is_valid() and contact_form.is_valid() and organizer_form.is_valid():
+            user = user_form.save()
+
+            contact_person = contact_form.save()
+
+            organizer = organizer_form.save(commit=False)
+            organizer.user = user
+            organizer.contact_person = contact_person
+            organizer.save()
+
+            return redirect(self.success_url)
+
+        return render(request, self.template_name, {
+            "user_form": user_form,
+            "contact_form": contact_form,
+            "organizer_form": organizer_form
+        })
+
+
