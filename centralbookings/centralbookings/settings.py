aecd6c9422594fd15c732b7681df292388eaead8
DavidTMN <davidtormon@gmail.com>
2025-11-29 02:00:09 +0800

    added main to installed apps

--- centralbookings/centralbookings/settings.py
+++ centralbookings/centralbookings/settings.py
@@ -39,11 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'UserManagement',
-<<<<<<< HEAD
-    'Profile',
-=======
     'main',
->>>>>>> Main-App
+    'Profile'
 ]
 
 MIDDLEWARE = [
