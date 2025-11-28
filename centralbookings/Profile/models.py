e6f2143707c2ea0235617199eb20f7209a31155f
Syd7 <sydpaguio@gmail.com>
2025-11-28 22:04:47 +0800

    removed unnecessary comment

--- centralbookings/Profile/models.py
+++ centralbookings/Profile/models.py
@@ -1,5 +1,4 @@
 """Create models Organizer & Activity with appropriate fields."""
-# In Profile/models.py (or any file in Profile)
-from UserManagement.models import Contact_Person, Organizer, Building, Location, Activity, Activity_Schedule, Department, Participant, Activity_Booking # whatever you need
+from UserManagement.models import Contact_Person, Organizer, Building, Location, Activity, Activity_Schedule, Department, Participant, Activity_Booking
 
 from django.db import models
