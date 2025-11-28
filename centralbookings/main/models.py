c115c39dbee5f7c68c148cbb8dd37d948c471979
DavidTMN <davidtormon@gmail.com>
2025-11-28 23:06:21 +0800

    edited models to reflect imports from UserManagement

---
+++ centralbookings/main/models.py
@@ -0,0 +1,3 @@
+from UserManagement.models import Contact_Person, Organizer, Building, Location, Activity, Activity_Schedule, Department, Participant, Activity_Booking
+
+from django.db import models
